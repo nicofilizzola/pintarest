fc80720b81c74cb904cae1d6edf7750d80878fc7
nicofilizzola <nicofilizzola.prod@gmail.com>
2021-02-16 11:19:46 +0100

    add show pages and navigation links

--- src/Controller/PinController.php
+++ src/Controller/PinController.php
@@ -6,11 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\PinRepository;
+use App\Entity\Pin;
 
 class PinController extends AbstractController
 {
     /**
-     * @Route("/", name="app_pin")
+     * @Route("/", name="app_home")
      */
     public function index(PinRepository $pinRepo): Response
     {
@@ -18,6 +19,17 @@ class PinController extends AbstractController
         return $this->render(
             'pin/index.html.twig',
             compact('allPins')
+        );
+    }
+
+    /**
+     * @Route("/pin/{id<\d+>}", name="app_pin_show")
+     */
+    public function show(Pin $pin): Response
+    {
+        return $this->render(
+            'pin/show.html.twig',
+            compact('pin')
         );
     }
 }
