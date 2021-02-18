6fd6dd97cca28e8f7a47a7f5b47ef0b60dcbfcc8
nicofilizzola <nicofilizzola.prod@gmail.com>
2021-02-18 20:36:46 +0100

    PinType form error messages setup

--- src/Entity/Pin.php
+++ src/Entity/Pin.php
@@ -2,14 +2,16 @@
 
 namespace App\Entity;
 
-use App\Entity\Traits\Timestampable;
-use App\Repository\PinRepository;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\PinRepository;
+use App\Entity\Traits\Timestampable;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=PinRepository::class)
  * @ORM\HasLifecycleCallbacks
  */
+
 class Pin
 {
     use Timestampable;
@@ -23,11 +25,15 @@ class Pin
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="Is there no title?")
+     * @Assert\Length(min=3, minMessage="So your title has less than 3 characters... That won't work")
      */
     private $title;
 
     /**
      * @ORM\Column(type="text")
+     * @Assert\NotBlank(message="Is there no description?")
+     * @Assert\Length(min=10, minMessage="Your pin's description must be at least 10 characters long")
      */
     private $description;
 
