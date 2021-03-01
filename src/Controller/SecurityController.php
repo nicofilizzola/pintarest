feee2a3ba73aa4f737716530ed4e738d1feb67e2
nicofilizzola <nicofilizzola.prod@gmail.com>
2021-03-01 11:28:50 +0100

    post method for logout and csrf validation

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,9 +2,10 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
@@ -28,10 +29,14 @@ class SecurityController extends AbstractController
     }
 
     /**
-     * @Route("/logout", name="app_logout")
+     * @Route("/logout", name="app_logout", methods="POST")
      */
-    public function logout()
+    public function logout(Request $req)
     {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+        $submittedToken = $req->request->get('_csrf_token');
+
+        if ($this->isCsrfTokenValid('logout', $submittedToken)){
+            throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+        }  
     }
 }
